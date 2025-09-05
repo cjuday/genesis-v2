31f4ce9e3d066552d521d23e85a339e5efb5bc4e
Taiammum Uday <taiammum.uday@bracits.com>
2025-09-05 23:41:25 +0600

    app index fixed

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,11 +16,27 @@
         <meta name="robots" content="index, follow">
         <title>Genesis - One Stop Business Solution in Bangladesh</title>
         @viteReactRefresh
-        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
         @vite(['resources/css/app.css','resources/js/app.jsx'])
         @inertiaHead
-        <link rel="stylesheet" href="{{ asset('css/font-awesome/all.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/fonts/google-fonts.css') }}">
+
+        <!-- Bootstrap (non-blocking) -->
+        <link rel="preload" as="style" href="{{ asset('css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" media="print" onload="this.media='all'">
+
+        <!-- Font Awesome (non-blocking) -->
+        <link rel="preload" as="style" href="{{ asset('css/font-awesome/all.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/font-awesome/all.min.css') }}" media="print" onload="this.media='all'">
+
+        <!-- Google Fonts (self-hosted, non-blocking) -->
+        <link rel="preload" as="style" href="{{ asset('css/fonts/google-fonts.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/fonts/google-fonts.css') }}" media="print" onload="this.media='all'">
+
+        <!-- Fallback for no-JS -->
+        <noscript>
+            <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+            <link rel="stylesheet" href="{{ asset('css/font-awesome/all.min.css') }}">
+            <link rel="stylesheet" href="{{ asset('css/fonts/google-fonts.css') }}">
+        </noscript>
     </head>
     <body>
         @inertia
